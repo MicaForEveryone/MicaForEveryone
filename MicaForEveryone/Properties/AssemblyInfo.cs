13297677e75039346986bcdf89fe234e134be9ef
Mohammad Amin Mollazadeh <madamin@pm.me>
2021-12-20 11:42:54 +0330

    Clean project files

--- MicaForEveryone/Properties/AssemblyInfo.cs
+++ MicaForEveryone/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyTitle("MicaForEveryone.UWP")]
+[assembly: AssemblyTitle("Mica For Everyone")]
 [assembly: AssemblyProduct("Mica For Everyone")]
 [assembly: AssemblyCompany("Minusium")]
 [assembly: AssemblyCopyright("Copyright (C) 2021 Mohammad Amin Mollazadeh")]
---
+++ Properties/AssemblyVersion.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2")]
