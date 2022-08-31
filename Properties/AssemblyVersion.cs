cfa18a840a7576a8b4ad5c350c27f8455836b7a9
Mohammad Amin Mollazadeh <madamin@pm.me>
2022-08-31 16:25:07 +0430

    version bump

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0.0")]
