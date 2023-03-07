a8b7f85c70ea6bbfc308fa2d562f1133bb705037
Meriç Bağlayan <baglayan19@itu.edu.tr>
2023-03-08 00:52:06 +0300

    feat(UI/Strings/tr): Add Turkish translation

--- MicaForEveryone.UI/SettingsView.xaml.cs
+++ MicaForEveryone.UI/SettingsView.xaml.cs
@@ -42,6 +42,7 @@ namespace MicaForEveryone.UI
             new Contributor("A-Loot", "https://github.com/A-Loot", "it"),
             new Contributor("MW1Z", "https://github.com/MW1Z", "zh-Hant"),
             new Contributor("seanardhana", "https://github.com/seanardhana", "id"),
+            new Contributor("Meriç Bağlayan", "https://github.com/baglayan", "tr"),
         };
 
         private void ListView_Loaded(object sender, RoutedEventArgs args)
