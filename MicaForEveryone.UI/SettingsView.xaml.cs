f51f65760d85649d7e36ff42bea9747d5cbfb1bc
Zan <62830223+Zan1456@users.noreply.github.com>
2023-03-15 04:50:39 +0100

    feat(UI/Strings/hu): Added Hungarian translation (#270)

--- MicaForEveryone.UI/SettingsView.xaml.cs
+++ MicaForEveryone.UI/SettingsView.xaml.cs
@@ -42,6 +42,7 @@ namespace MicaForEveryone.UI
             new Contributor("A-Loot", "https://github.com/A-Loot", "it"),
             new Contributor("MW1Z", "https://github.com/MW1Z", "zh-Hant"),
             new Contributor("seanardhana", "https://github.com/seanardhana", "id"),
+            new Contributor("Zan1456", "https://github.com/Zan1456", "hu"),
             new Contributor("Meriç Bağlayan", "https://github.com/baglayan", "tr"),
         };
 
