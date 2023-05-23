c04a19bfcd71fea5d9ef9495840b21f2c42855de
chefkozaki <122468057+chefkozaki@users.noreply.github.com>
2023-05-23 15:01:48 +0700

    Update SettingsView.xaml.cs

--- MicaForEveryone.UI/SettingsView.xaml.cs
+++ MicaForEveryone.UI/SettingsView.xaml.cs
@@ -44,6 +44,7 @@ namespace MicaForEveryone.UI
             new Contributor("seanardhana", "https://github.com/seanardhana", "id"),
             new Contributor("Zan1456", "https://github.com/Zan1456", "hu"),
             new Contributor("Meriç Bağlayan", "https://github.com/baglayan", "tr"),
+            new Contributor("ChefKozaki", "https://github.com/chefkozaki", "vi"),
         };
 
         private void ListView_Loaded(object sender, RoutedEventArgs args)
