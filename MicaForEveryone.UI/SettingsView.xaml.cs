c4bd215a9251fd2818a05fb06e6c42aa3a7be290
thepoladov13 <thepoladov@protonmail.com>
2023-07-05 00:23:35 -0700

    Update SettingsView.xaml.cs

--- MicaForEveryone.UI/SettingsView.xaml.cs
+++ MicaForEveryone.UI/SettingsView.xaml.cs
@@ -25,7 +25,7 @@ namespace MicaForEveryone.UI
 
         internal Contributor[] Translators { get; } =
         {
-            new Contributor("Merhab Poladov", "https://github.com/thepoladov13", "az"),
+            new Contributor("Mehrab Poladov", "https://github.com/thepoladov13", "az"),
             new Contributor("uDEV2019", "https://github.com/uDEV2019", "de"),
             new Contributor("Amaro Martínez", "https://github.com/xoascf", "es"),
             new Contributor("SaiyajinK", "https://github.com/SaiyajinK", "fr"),
