4f2dad4ee14acacd1c7491a9c107ab16003b1733
Dongle <29563098+dongle-the-gadget@users.noreply.github.com>
2023-07-15 12:39:22 +0700

    Bump to 1.3.1.2

--- Properties/AssemblyVersion.cs
+++ Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.1.1")]
-[assembly: AssemblyFileVersion("1.3.1.1")]
-[assembly: AssemblyInformationalVersion("1.3.1.1")]
+[assembly: AssemblyVersion("1.3.1.2")]
+[assembly: AssemblyFileVersion("1.3.1.2")]
+[assembly: AssemblyInformationalVersion("1.3.1.2")]
