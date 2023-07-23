2ebd8f6af2c376872a247c7aaa3f683897c978a0
maggster165 <69034405+maggster165@users.noreply.github.com>
2023-07-23 19:55:43 +0200

    add maggster165 to translators list

--- MicaForEveryone.UI/SettingsView.xaml.cs
+++ MicaForEveryone.UI/SettingsView.xaml.cs
@@ -25,6 +25,7 @@ namespace MicaForEveryone.UI
 
         internal Contributor[] Translators { get; } =
         {
+            new Contributor("maggster165", "https://github.com/maggster165", "cs"),
             new Contributor("Merhab Poladov", "https://github.com/thepoladov13", "az"),
             new Contributor("uDEV2019", "https://github.com/uDEV2019", "de"),
             new Contributor("Amaro Martínez", "https://github.com/xoascf", "es"),
